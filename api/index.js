ced1f4c88184e21eec3ab405133f0d16e192e8fc
Chintan Prajapati <chintan9@student.fdu.edu>
2019-12-18 21:27:42 +0000

    reformat update

--- api/index.js
+++ api/index.js
@@ -14,132 +14,132 @@ const CONTENT_TYPES_IMAGE = ['image/jpeg', 'image/png']
 let tfModelCache
 
 async function loadModel() {
-  try {
-    const tf = await loadTf()
+    try {
+        const tf = await loadTf()
 
-    if (tfModelCache) {
-      return tfModelCache
+        if (tfModelCache) {
+            return tfModelCache
+        }
+
+        tfModelCache = await tf.loadGraphModel(`${TF_MODEL_URL}/model.json`)
+        return tfModelCache
+    } catch (err) {
+        console.log(err)
+        throw BadRequestError('The model could not be loaded')
     }
-
-    tfModelCache = await tf.loadGraphModel(`${TF_MODEL_URL}/model.json`)
-    return tfModelCache
-  } catch (err) {
-    console.log(err)
-    throw BadRequestError('The model could not be loaded')
-  }
 }
 
 async function imgToTensor(imgBuffer) {
-  const tf = await loadTf()
+    const tf = await loadTf()
 
-  const image = await Jimp.read(imgBuffer)
-  const { width, height, data } = image.bitmap
+    const image = await Jimp.read(imgBuffer)
+    const { width, height, data } = image.bitmap
 
-  const numPixels = width * height
-  const values = new Int32Array(numPixels * 3)
+    const numPixels = width * height
+    const values = new Int32Array(numPixels * 3)
 
-  for (let i = 0; i < numPixels; i++) {
-    for (let c = 0; c < 3; c++) {
-      values[i * 3 + c] = data[i * 4 + c]
+    for (let i = 0; i < numPixels; i++) {
+        for (let c = 0; c < 3; c++) {
+            values[i * 3 + c] = data[i * 4 + c]
+        }
     }
-  }
 
-  const tensor = await tf.tidy(() => tf.tensor3d(values, [height, width, 3]))
+    const tensor = await tf.tidy(() => tf.tensor3d(values, [height, width, 3]))
 
-  return { tensor, width, height }
+    return { tensor, width, height }
 }
 
 async function predict(tfModel, tensor) {
-  const tf = await loadTf()
+    const tf = await loadTf()
 
-  const batched = await tf.tidy(() => tensor.expandDims())
-  const result = await tfModel.executeAsync(batched)
-  const scores = result[0].arraySync()[0]
-  const boxes = result[1].dataSync()
+    const batched = await tf.tidy(() => tensor.expandDims())
+    const result = await tfModel.executeAsync(batched)
+    const scores = result[0].arraySync()[0]
+    const boxes = result[1].dataSync()
 
-  batched.dispose()
-  tf.dispose(result)
+    batched.dispose()
+    tf.dispose(result)
 
-  return { scores, boxes }
+    return { scores, boxes }
 }
 
 function findMax(array) {
-  return array.reduce((max, item, index) => {
-    if (max.item === undefined || item > max.item) {
-      return { item, index }
-    }
-    return max
-  }, {})
+    return array.reduce((max, item, index) => {
+        if (max.item === undefined || item > max.item) {
+            return { item, index }
+        }
+        return max
+    }, {})
 }
 
 async function tensorsToBBoxes({ scores, boxes, width, height }) {
-  const tf = await loadTf()
+    const tf = await loadTf()
 
-  const maxScores = scores.map(findMax)
+    const maxScores = scores.map(findMax)
 
-  const indexTensor = tf.tidy(() => {
-    const boxes2 = tf.tensor2d(boxes, [1917, 4])
-    return tf.image.nonMaxSuppression(
-      boxes2,
-      maxScores.map(x => x.item),
-      20,
-      0.5,
-      0.5
-    )
-  })
+    const indexTensor = tf.tidy(() => {
+        const boxes2 = tf.tensor2d(boxes, [1917, 4])
+        return tf.image.nonMaxSuppression(
+            boxes2,
+            maxScores.map(x => x.item),
+            20,
+            0.5,
+            0.5
+        )
+    })
 
-  const indexes = indexTensor.arraySync()
-  indexTensor.dispose()
+    const indexes = indexTensor.arraySync()
+    indexTensor.dispose()
 
-  const bboxes = indexes.map(index => {
-    return {
-      bbox: [
-        { x: boxes[index * 4 + 1] * width, y: boxes[index * 4] * height },
-        {
-          x: boxes[index * 4 + 3] * width,
-          y: boxes[index * 4 + 2] * height
+    const bboxes = indexes.map(index => {
+        return {
+            bbox: [
+                { x: boxes[index * 4 + 1] * width, y: boxes[index * 4] * height },
+                {
+                    x: boxes[index * 4 + 3] * width,
+                    y: boxes[index * 4 + 2] * height
+                }
+            ],
+            score: maxScores[index].item,
+            class: CLASSES[maxScores[index].index + 1].displayName
         }
-      ],
-      score: maxScores[index].item,
-      class: CLASSES[maxScores[index].index + 1].displayName
-    }
-  })
+    })
 
-  return bboxes
+    return bboxes
 }
 
 module.exports = cors(
-  handleError(async (req, res) => {
-    if (req.method === 'OPTIONS') {
-      return send(res, 200)
-    }
+    handleError(async (req, res) => {
+        if (req.method === 'OPTIONS') {
+            return send(res, 200)
+        }
 
-    if (req.url === '/api/warm') {
-      await loadTf()
-      await loadModel()
-      return send(res, 200)
-    }
+        if (req.url === '/api/warm') {
+            await loadTf()
+            await loadModel()
+            return send(res, 200)
+        }
 
-    if (req.url === '/api/predict') {
-      const { type: mimeType } = contentType.parse(req)
+        if (req.url === '/api/predict') {
+            const { type: mimeType } = contentType.parse(req)
 
-      if (CONTENT_TYPES_IMAGE.includes(mimeType)) {
-        const tf = await loadTf()
-        const tfModel = await loadModel()
-        
-        const buf = await buffer(req, { limit: '5mb' })
-        const { tensor, width, height } = await imgToTensor(buf)
+            if (CONTENT_TYPES_IMAGE.includes(mimeType)) {
+                const tf = await loadTf()
+                const tfModel = await loadModel()
 
-        const { scores, boxes } = await predict(tfModel, tensor)
+                const buf = await buffer(req, { limit: '5mb' })
+                const { tensor, width, height } = await imgToTensor(buf)
 
-        const bboxes = await tensorsToBBoxes({ scores, boxes, width, height })
+                const { scores, boxes } = await predict(tfModel, tensor)
 
-        return send(res, 200, bboxes)
-      }
+                const bboxes = await tensorsToBBoxes({ scores, boxes, width, height })
 
-      throw BadRequestError('Only images are supported at the moment')
-    }
+                return send(res, 200, bboxes)
+            }
 
-    return send(res, 404)
-  })
+            throw BadRequestError('Only images are supported at the moment')
+        }
+
+        return send(res, 404)
+    })
 )
--- api/lib/error.js
+++ api/lib/error.js
@@ -1,15 +1,15 @@
 const { send } = require('micro')
 
 class ClientError extends Error {
-  constructor(statusCode, error) {
-    super(`${error.message} (${error.code})`)
-    this.statusCode = statusCode
-    this.error = error
-  }
+    constructor(statusCode, error) {
+        super(`${error.message} (${error.code})`)
+        this.statusCode = statusCode
+        this.error = errors
+    }
 
-  send(res) {
-    send(res, this.statusCode, { error: this.error })
-  }
+    send(res) {
+        send(res, this.statusCode, { error: this.error })
+    }
 }
 
 /**
@@ -17,51 +17,51 @@ class ClientError extends Error {
  * @param {{ code: string, message: string, [detail: string]: any }} error
  */
 const createError = (statusCode, error, details) => {
-  // legacy createError
-  if (typeof error === 'string') {
-    return new ClientError(statusCode, { message: error, details })
-  }
+    // legacy createError
+    if (typeof error === 'string') {
+        return new ClientError(statusCode, { message: error, details })
+    }
 
-  return new ClientError(statusCode, error)
+    return new ClientError(statusCode, error)
 }
 
 const InternalServerError = () =>
-  createError(500, {
-    code: 'internal_server_error',
-    message: 'An unexpected internal server error occurred'
-  })
+    createError(500, {
+        code: 'internal_server_error',
+        message: 'An unexpected internal server error occurred'
+    })
 
 const NotAuthorizedError = () =>
-  createError(401, {
-    code: 'not_authorized',
-    message: 'Not authorized'
-  })
+    createError(401, {
+        code: 'not_authorized',
+        message: 'Not authorized'
+    })
 
 const BadRequestError = (message = 'Request is malformed') =>
-  createError(400, {
-    code: 'bad_request',
-    message
-  })
+    createError(400, {
+        code: 'bad_request',
+        message
+    })
 
 const handleError = fn => async (req, res, opts) => {
-  try {
-    return await fn(req, res, opts)
-  } catch (error) {
-    // eslint-disable-next-line
-    console.error(error)
+    try {
+        return await fn(req, res, opts)
+    } catch (error) {
+        // eslint-disable-next-line
+        console.error(error)
 
-    if (error instanceof ClientError) {
-      error.send(res)
-    } else {
-      InternalServerError().send(res)
+        if (error instanceof ClientError) {
+            error.send(res)
+        } else {
+            InternalServerError().send(res)
+        }
     }
-  }
 }
 
 module.exports = {
-  handleError,
-  createError,
-  InternalServerError,
-  NotAuthorizedError,
-  BadRequestError
+    handleError,
+    createError,
+    InternalServerError,
+    NotAuthorizedError,
+    BadRequestError
 }
